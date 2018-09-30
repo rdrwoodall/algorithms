19dec72c5d2ba54bdb0ace86548bbec057b58d8c
Robert Woodall <rdrwoodall@gmail.com>
2018-09-30 11:53:00 -0500

    LinkedList: create add/addFront methods and tests

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,13 +1,56 @@
+const ListNode = require('./list-node');
+
 class LinkedList {  
   constructor() {
     this.head = null;
+    this.tail = null;
   }
 
-  // TODO: implement following methods:
-  // addFront
+  add(value) {
+    if (!value) { // no value
+      this.handleValueError('add');
+    }
+
+    const node = new ListNode(value);
+
+    if (!this.head) { // empty list
+      this.head = node;
+      this.tail = node;
+      return this.head;
+    }
+    
+    // add node to end
+    const prevTail = this.tail;
+    prevTail.next = node;
+    this.tail = node;
+  }
+
+  addFront(value) {
+    if (!value) {
+      this.handleValueError('addFront');
+    }
+
+    const node = new ListNode(value);
+    this.head = node;
+    return this.head;
+  }
+
+  removeFront() {
+    if (!this.head) {
+      return null;
+    }
+
+    this.head = this.head.next;
+    return this.head;
+  }
+
+  // TODO: implement following methods and tests:
   // removeFront
   // contains
-  // front
+
+  handleValueError(methodName) {
+    throw new Error(`${method} expectas a value as an argument`)
+  }
 }
 
 module.exports = LinkedList;
--- linked-list/linked-list.test.js
+++ linked-list/linked-list.test.js
@@ -1,6 +1,69 @@
 const LinkedList = require('./linked-list');
 
-test('instantiates an instance of LinkedList', () => {
-  const actual = new LinkedList();
-  expect(actual).toBeInstanceOf(LinkedList);
-})
+describe('LinkedList instance', () => {
+  test('instantiates an instance of LinkedList', () => {
+    const list = new LinkedList();
+    expect(list).toBeInstanceOf(LinkedList);
+  });
+
+  test('a new instance defines a null head and tail property', () => {
+    const list = new LinkedList();
+    expect(list.head).toBeNull();
+    expect(list.tail).toBeNull();
+  });
+});
+
+describe('LinkedList.add method', () => {
+  test('throws an Error if no value is passed as an argument', () => {
+    const list = new LinkedList();
+    expect(list.addFront).toThrow(Error);
+  });
+
+  test('creates a new node and assigns it to the tail property', () => {
+    const list = new LinkedList();
+    
+    list.add(1);
+    expect(list.head).toBe(list.tail); // 1 node in list
+    expect(list.head).not.toBeNull();
+    expect(list.tail).not.toBeNull();
+
+    list.add(2);
+    expect(list.head).not.toBe(list.tail);
+    expect(list.head.value).toBe(1);
+    expect(list.tail.value).toBe(2);
+
+    list.add(3);
+    expect(list.head.value).toBe(1);
+    expect(list.tail.value).toBe(3);
+  });
+});
+
+describe('LinkedList.addFront method', () => {
+  test('throws an Error if no value is passed as an argument', () => {
+    const list = new LinkedList();
+    expect(list.addFront).toThrow(Error);
+  });
+
+  test('creates a new ListNode and assigns it to head property', () => {
+    const expected = 1;
+    const list = new LinkedList();
+    const head = list.addFront(expected);
+    
+    expect(list.head).toBe(head);
+    expect(list.head.value).toBe(expected);
+    expect(list.head.next).toBeNull();
+  });
+});
+
+describe('LinkedList.removeFront method', () => {
+  test('returns null if the list is empty', () => {
+    const list = new LinkedList();
+    const head = list.removeFront();
+    expect(head).toBeNull();
+  });
+
+  test('removes the front item and assigns head to the next node', () => {
+    const list = new LinkedList();
+  });
+});
+
