6aa98d71965b09573a6728f095fad5b1c1e6310b
Robert Woodall <rdrwoodall@gmail.com>
2018-10-30 22:11:10 -0500

    binary-search-tree: enhance arrayToBst to handle odd length arrays

--- binary-search-trees/binary-tree-utils.js
+++ binary-search-trees/binary-tree-utils.js
@@ -29,6 +29,7 @@ class BinaryTreeUtils {
     // local helpers
     const validLeftIndex = index => index >= 0;
     const validRightIndex = index => index <= (arr.length - 1);
+    const oddLengthArray = () => (arr.length % 2) !== 0;
 
     const mid = arr.length / 2;
     const bst = new BinarySearchTree();
@@ -42,13 +43,31 @@ class BinaryTreeUtils {
     let leftIndex = mid - 1;
     let rightIndex = mid;
 
-    while(validLeftIndex || validRightIndex) {
-      if (validLeftIndex) {
+    // for odd length arrays
+    // need to determine which side to start adding values from
+    // this will be the 'half' with the most elements
+    // this step is important to keep the tree height balanced
+    if (oddLengthArray) {
+      const leftCount = mid - left;
+      const rightCount = arr.length - mid;
+      const startIndex = Math.max(leftCount, rightCount);
+
+      if (startIndex === leftIndex) {
+        // add item and decrement left index
+        bst.add(arr[leftIndex--]);
+      } else {
+        // add item and increment right index
+        bst.add(arr[rightIndex++]);
+      } 
+    }
+
+    while(validLeftIndex(leftIndex) || validRightIndex(rightIndex)) {
+      if (validLeftIndex(leftIndex)) {
         // add item and decrement left index
         bst.add(arr[leftIndex--]);
       }
 
-      if (validRightIndex) {
+      if (validRightIndex(rightIndex)) {
         // add item and increment right index
         bst.add(arr[rightIndex++]);
       }
